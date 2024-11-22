a6160dcd513e5537177cbef3619f38218b394ff4
Kyle Thompson <140655142+KyleThompson1@users.noreply.github.com>
2024-11-22 14:36:08 -0600

    Registration works, Admin Panel works, Banning accounts works, User accounts are setup.

--- csi3335f2024.py
+++ csi3335f2024.py
@@ -1,7 +1,7 @@
 # Database configuration info in mysql dict
 mysql = {
     'host': 'localhost',
-    'user': 'root',
-    'password': 'csi3335rocks',
+    'user': 'root',             # May have to change to work on your local machine
+    'password': 'csi3335rocks', # May have to change to work on your local machine --- #'csi3335rocks'
     'database': 'Tigers'
 }
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+from DatabaseSetup import initialize_database
 from app import create_app
 
 app = create_app()
+
+# Initialize the database
+initialize_database()  # Ensures the Tigers database and users table are set up
 
 if __name__ == '__main__':
     app.run(debug=True)
