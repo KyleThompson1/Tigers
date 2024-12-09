ecc3e38044819232971d1d2d76d7498eea1ddf9a
Kyle Thompson <140655142+KyleThompson1@users.noreply.github.com>
2024-12-08 23:27:42 -0600

    Updated 2023 stats using Tigers.sql from zip file that must be added to project

--- csi3335f2024.py
+++ csi3335f2024.py
@@ -1,7 +1,7 @@
 # Database configuration info in mysql dict
 mysql = {
     'host': 'localhost',
-    'user': 'markjosephs',             # May have to change to work on your local machine
-    'password': 'password', # May have to change to work on your local machine --- #'csi3335rocks'
-    'database': 'baseball'
+    'user': 'root',             # May have to change to work on your local machine
+    'password': 'muffinsncake', # May have to change to work on your local machine --- #'csi3335rocks'
+    'database': 'tigers'
 }
